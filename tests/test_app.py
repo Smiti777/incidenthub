52ebb389b8641c75fe2fe1a42bc7b76f08ce4809
Smiti777 <tour201607@gmail.com>
2026-09-25 23:18:29 +0530

    Fix failing CI test

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from app import app
 def test_homepage():
     client = app.test_client()
     response = client.get("/")
-    assert response.status_code == 201
+    assert response.status_code == 200
 
 
 def test_create_page():
